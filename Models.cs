3fbd7c284b20d2999c85d49d6d6a710bbadf721b
elifeinshtein <tiglynx@gmail.com>
2023-02-19 12:14:46 +0200

    -A

---
+++ Models.cs
@@ -0,0 +1,14 @@
+﻿namespace sample
+{
+    public class Thumbnail
+    {
+        public string ThumbnailPic;
+        public string VideoTime;
+        public string ChannelPic;
+        public string VideoTitle;
+        public string VideoAuthor;
+        public int ViewCount;
+        public string VideoStats;
+        
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿var minSecInterval = 5;
+var maxSecInterval = 10;
+var minPercentage = 1;
+var maxPercentage = 5;
+var ViewCount = 1;
+
+function findPercentage(min, max) {
+    return ((Math.random() * (max - min + 1) + min) / 100);
+}
+
+function myFunction() {
+
+    var arr = document.querySelectorAll(".video-views");
+
+    arr.forEach(ViewCount => {
+        var numOfViews = parseInt(ViewCount.innerHTML);
+        numOfViews = Math.ceil(numOfViews * (1 + findPercentage(minPercentage, maxPercentage)));
+        ViewCount.innerHTML = numOfViews;
+    })
+   
+}
+
+var rand = Math.floor(Math.random() * (maxSecInterval - minSecInterval + 1) + minSecInterval); //Generate Random number between 5 - 10
+setInterval(myFunction, rand * 1000);
