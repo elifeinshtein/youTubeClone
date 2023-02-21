64ce2d944eb6163e6ddebca8b9b56651364bee3e
elifeinshtein <tiglynx@gmail.com>
2023-02-21 10:29:09 +0200

    what you think about it now

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,30 +1,12 @@
 ﻿
 let Range = (start, multiplier) => Math.floor(start + Math.random() * start * (multiplier - 1));
 
-function intToString(num) {
-    num = num.toString().replace(/[^0-9.]/g, '');
-    if (num < 1000) {
-        return num;
-    }
-    let si = [
-        { v: 1E3, s: "K" },
-        { v: 1E6, s: "M" },
-        { v: 1E9, s: "B" },
-        { v: 1E12, s: "T" },
-        { v: 1E15, s: "P" },
-        { v: 1E18, s: "E" }
-    ];
-    let index;
-    for (index = si.length - 1; index > 0; index--) {
-        if (num >= si[index].v) {
-            break;
-        }
-    }
-    return (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[index].s;
-}
+
 
 setInterval(() => document.querySelectorAll(".video-views").forEach(view => {
-    view.innerHTML = (intToString(Range(parseInt(view.innerHTML) + 5, 1.05)));
+    view.innerHTML = numeral(Range(parseFloat(view.innerHTML) + 5, 1.05)).format("0.000a");
+    console.log(parseFloat(view.innerHTML));
+
 }), Range(1000, 10));
 
 
