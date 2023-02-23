cc2830dc0dec50adc8ad85a986e263bfa6ab9b71
elifeinshtein <tiglynx@gmail.com>
2023-02-23 10:11:31 +0200

    'working now on how to read number with comma

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -24,33 +24,51 @@ function FormatLongNumber(value) {
 }
 
 function replace(str) {
-    
-    if( str.includes(',')) {
-
-        return str.replaceAll(/,/g, "");
-    }
-
-    else if (str.includes('K')) {
-        return str.replaceAll(/K/g, '000');
+ 
+     if (str.includes('K')) {
+         return 1000 * parseFloat(str);
     }
 
     else if (str.includes('M')) {
-        return str.replaceAll(/M/g, '000000');
+         return 1000000 * parseFloat(str);
     }
 
     else if (str.includes('B')) {
-        return str.replaceAll(/B/g, '000000000');
+         return 1000000000 * parseFloat(str);
     }
 
     else
-        return str;
+         return 1 *parseFloat(Number(str));
 }
 
-setInterval(() => document.querySelectorAll(".video-views").forEach(view => {
 
-    view.innerHTML = FormatLongNumber(Range(Number(view.innerHTML) + 5, 1.05));
-    
-    parseInt(replace(view.innerHTML));
+let IntervalFrequency = Range(1000, 3);
+console.log(`The interval frequency is: ${IntervalFrequency}ms`);
+console.log("-------------------------------"); // just creating an empty line
 
-}), Range(1000, 10));
+let Iteration = 0;
+setInterval(function () {
+    let ElementsToProcess = 6; 
 
+    console.log(`The function inside of setInterval is being called, current iteration: ${Iteration++}`);
+
+    document.querySelectorAll(".video-views").forEach(view => {
+        if (--ElementsToProcess < 0) return;
+
+        let current = view.innerHTML;
+        console.log(`The current value is: ${current}`);
+
+        let replaced = replace(current);
+        console.log(`The value after being cleaned from letters: ${replaced}`);
+
+        let number = Number(replaced); // turning the "clean value" into a number.
+        number = Range(number + 5, 1.05);
+        console.log(`The incremented number: ${number}`);
+
+        let formatted = FormatLongNumber(number);
+        console.log(`The formatted number: ${formatted}`);
+
+        view.innerHTML = formatted;
+    });
+    console.log("-------------------------------"); // just creating an empty line
+}, IntervalFrequency);
